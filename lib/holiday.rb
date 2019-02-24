986f30c5a3b263cb4551b82d5d1044e0a119ec79
Darcy Linde <lindedarcy@gmail.com>
2019-02-24 03:30:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,6 +89,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_string = holiday_array.join(" ")
       holiday_string += ": "
       holiday_string += supply.join(" ")
+      puts holiday_string
     end
   end
 
