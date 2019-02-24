389e01a4fb4e68da57e3e66ca6eb8646cf687d5f
Darcy Linde <lindedarcy@gmail.com>
2019-02-24 03:13:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     season.each do |holiday, supply|
+      puts "  "
       
     end
   end
